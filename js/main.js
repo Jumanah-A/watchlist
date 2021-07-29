f68fcc0a1219536446bb9346e6b809ac2c49a268
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-29 11:08:48 -0700

    Grabbed the data from the dom tree and displayed it to the user when searching a title

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
-function handleSubmit(event)
-{
+function handleSubmit(event) {
   event.preventDefault();
   var movieName = document.querySelector('.title').value;
-  getMovieData(movieName.split(" ").join("+"));
+  getMovieData(movieName.split(' ').join('+'));
+  switchViews('movie-view');
 }
-document.querySelector('form').addEventListener('submit',handleSubmit);
+document.querySelector('form').addEventListener('submit', handleSubmit);
 
 function getMovieData(name) {
   var xhr = new XMLHttpRequest();
+  var movieData;
   xhr.open('GET', 'https://www.omdbapi.com/?apikey=d0b53caa&t=' + name);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    console.log(xhr.response);
+    addMovie(xhr.response);
   });
   xhr.send();
+  return movieData;
+}
+
+function addMovie(movieObject) {
+  document.querySelector('.movie-title').textContent = movieObject.Title;
+  document.querySelector('.year').textContent = movieObject.Year;
+  document.querySelector('.genre').textContent = movieObject.Genre;
+  document.querySelector('.imdb-rating').textContent = movieObject.imdbRating;
+  document.querySelector('.plot').textContent = movieObject.Plot;
+  document.querySelector('.poster').setAttribute('src', movieObject.Poster);
+}
+
+function switchViews(view) {
+  var $viewList = document.querySelectorAll('.view');
+  if (view === 'search-view') {
+    document.querySelector('.header').className = 'header hidden';
+    document.querySelector('body').className = 'background-image';
+  } else {
+    document.querySelector('body').className = 'background-color';
+    document.querySelector('.header').className = 'header';
+    for (var i = 0; i < $viewList.length; i++) {
+      if (view !== $viewList[i].getAttribute('data-view')) {
+        $viewList[i].className = 'view hidden';
+      } else {
+        $viewList[i].className = 'view';
+      }
+    }
+  }
+
 }
